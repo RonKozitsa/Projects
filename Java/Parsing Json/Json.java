962b840dd40f7be39b4b012de03876c6293d56b2
rkozitsa <rkozitsa@paloaltonetworks.com>
2019-03-26 22:35:06 +0200

    Graphics added

--- Java/Parsing Json/Json.java
+++
@@ -1,7 +0,0 @@
-public class Json {
-    public static void main(String[] args) {
-        JsonWrite.WriteJson("hello , this is my message");
-        JsonRead.ReadJson("myJsonFile.json");
-
-    }
-}
