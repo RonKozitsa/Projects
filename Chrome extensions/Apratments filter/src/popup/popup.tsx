3f24e3bd93a354f527267154f3bd801dac4a4eb2
rkozitsa <rkozitsa@paloaltonetworks.com>
2022-06-05 15:37:54 +0100

    fix extension

--- Chrome extensions/Apratments filter/src/popup/popup.tsx
+++ Chrome extensions/Apratments filter/src/popup/popup.tsx
@@ -53,13 +53,28 @@ const updateHiddenCount = () => {
     document.getElementById('hidden-count').innerText = numberOfHiddenApartments;
 }
 
-setNumberOfHiddenApartments();
+const render = () =>{
+    chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+        const url = tabs[0].url;
+        if (!(url.includes(Websites.onTheMarket) || url.includes(Websites.rightMove))) {
+            root.render(
+                <>
+                    <span className="empty-message">Please visit RightMove or OnTheMarket to use this extension</span>
+                </>
+            );
+        } else {
+            const currentSite = url.includes(Websites.onTheMarket) ? Websites.onTheMarket : Websites.rightMove;
+            root.render(
+                <>
+                    <span>Apartments Filter - {currentSite}</span>
+                    <button onClick={removeApartment}>Dont show this apartment again</button>
+                    <div>Hidden apartments : <span id="hidden-count">{numberOfHiddenApartments}</span></div>
+                    <button onClick={resetList}>Reset List</button>
+                </>
+            );
+        }
+    });
+}
 
-root.render(
-    <>
-        <span>Apartments Filter</span>
-        <button onClick={removeApartment}>Dont show this apartment again</button>
-        <div>Hidden apartments : <span id="hidden-count">{numberOfHiddenApartments}</span></div>
-        <button onClick={resetList}>Reset List</button>
-    </>
-);
+setNumberOfHiddenApartments();
+render();
