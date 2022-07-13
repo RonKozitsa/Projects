a0f7eb8bf368589d12b8392f370c10f07582a97b
rkozitsa <rkozitsa@paloaltonetworks.com>
2022-07-13 22:15:46 +0100

    remove other

--- other/script.js
+++
@@ -1,8 +0,0 @@
-<script>
-  const body = document.createElement("body");
-  const iframe = document.createElement("iframe");
-  iframe.src = "https://drive.google.com/file/d/1IzrxFmPyywdoh_ucHRpdfdjHC8FRXwaC/preview";
-  body.style = "display: flex; align-items:center;justify-content: center;height: 100vh;width: 100vw;";
-  body.appendChild(iframe);
-  document.body = body;
-</script>
